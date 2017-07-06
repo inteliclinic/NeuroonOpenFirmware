e7609c525c59dabeff526a2365357e0fd40298cc
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-07-06 14:09:26 +0200

    add unregister SPI function

--- src/ic_driver_spi.h
+++ src/ic_driver_spi.h
@@ -12,7 +12,7 @@
 
 #include "nrf_drv_spi.h"
 
-#define REGISTER_TO_SPI(name, SS_PIN, code)\
+#define SPI_REGISTER(name, SS_PIN, code)\
   static const nrf_drv_spi_t spi_instance_##name = NRF_DRV_SPI_INSTANCE(0);\
   void name##_spi_IRQ_handle(nrf_drv_spi_evt_t const * p_event){UNUSED_PARAMETER(p_event); code;};\
   ic_spi_init(&spi_instance_##name, SS_PIN, name##_spi_IRQ_handle);
@@ -20,6 +20,9 @@
 #define SPI_SEND_DATA(name, in_buffer, out_buffer, len)\
   nrf_drv_spi_transfer(&spi_instance_##name,\
       (uint8_t *)in_buffer, (uint8_t)len, (uint8_t *)out_buffer, (uint8_t)len);
+
+#define SPI_UNREGISTER(name)\
+  nrf_drv_spi_uninit(&spi_instance_##name);
 
 void ic_spi_init(const nrf_drv_spi_t *const instance, uint8_t pin, void(*func)());
 
