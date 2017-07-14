50c834be11f2fb3e0898099134c6e9bce7b9d385
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-07-14 12:08:54 +0200

    Separate spi registration and initialization

--- src/ic_driver_spi.c
+++ src/ic_driver_spi.c
@@ -15,11 +15,11 @@
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 
-void ic_spi_init(const nrf_drv_spi_t *const instance, uint8_t pin, void(*func)()){
+void ic_spi_init(const nrf_drv_spi_t *const instance, uint8_t pin, void(*callback)()){
     nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
     spi_config.ss_pin   = pin;
     spi_config.miso_pin = IC_SPI_MISO_PIN;
     spi_config.mosi_pin = IC_SPI_MOSI_PIN;
     spi_config.sck_pin  = IC_SPI_SCK_PIN;
-    APP_ERROR_CHECK(nrf_drv_spi_init(instance, &spi_config, func));
+    APP_ERROR_CHECK(nrf_drv_spi_init(instance, &spi_config, callback));
 }
--- src/ic_driver_spi.h
+++ src/ic_driver_spi.h
@@ -12,18 +12,21 @@
 
 #include "nrf_drv_spi.h"
 
-#define SPI_REGISTER(name, SS_PIN, code)\
-  static const nrf_drv_spi_t spi_instance_##name = NRF_DRV_SPI_INSTANCE(IC_SPI_INSTANCE);\
-  void name##_spi_IRQ_handle(nrf_drv_spi_evt_t const * p_event){UNUSED_PARAMETER(p_event); code;};\
-  ic_spi_init(&spi_instance_##name, SS_PIN, name##_spi_IRQ_handle);
+#define SPI_REGISTER(name, ss_pin, code)\
+  static const nrf_drv_spi_t name##_spi_instance = NRF_DRV_SPI_INSTANCE(IC_SPI_INSTANCE);\
+  static const uint8_t name##_ss_pin = ss_pin;\
+  void name##_spi_IRQ_handle(nrf_drv_spi_evt_t const * p_event){UNUSED_PARAMETER(p_event); code;}
+
+#define SPI_INIT(name)\
+  ic_spi_init(name##_spi_instance, name##_ss_pin, name##_spi_IRQ_handle)
 
 #define SPI_SEND_DATA(name, in_buffer, out_buffer, len)\
-  nrf_drv_spi_transfer(&spi_instance_##name,\
+  nrf_drv_spi_transfer(&name##_spi_instance,\
       (uint8_t *)in_buffer, (uint8_t)len, (uint8_t *)out_buffer, (uint8_t)len);
 
-#define SPI_UNREGISTER(name)\
-  nrf_drv_spi_uninit(&spi_instance_##name);
+#define SPI_UNINIT(name)\
+  nrf_drv_spi_uninit(&name##_spi_instance);
 
-void ic_spi_init(const nrf_drv_spi_t *const instance, uint8_t pin, void(*func)());
+void ic_spi_init(const nrf_drv_spi_t *const instance, uint8_t pin, void(*callback)());
 
 #endif /* !IC_DRIVER_SPI_H */
