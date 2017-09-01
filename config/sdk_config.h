8b81bbe011dba2d460cc3405ea4cfb2a96567f4e
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-09-01 16:50:57 +0200

    Disable SDK RTC

--- config/sdk_config.h
+++ config/sdk_config.h
@@ -1735,7 +1735,7 @@
 // <e> RTC_ENABLED - nrf_drv_rtc - RTC peripheral driver
 //==========================================================
 #ifndef RTC_ENABLED
-#define RTC_ENABLED 1
+#define RTC_ENABLED 0
 #endif
 #if  RTC_ENABLED
 // <o> RTC_DEFAULT_CONFIG_FREQUENCY - Frequency  <16-32768>
@@ -1783,7 +1783,7 @@
 
 
 #ifndef RTC2_ENABLED
-#define RTC2_ENABLED 1
+#define RTC2_ENABLED 0
 #endif
 
 // <o> NRF_MAXIMUM_LATENCY_US - Maximum possible time[us] in highest priority interrupt
