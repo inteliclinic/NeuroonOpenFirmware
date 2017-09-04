3226d077f4753612f20b7fa566ba813f1e371ac0
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-09-04 16:36:53 +0200

    add uninit and fixed device address

--- src/ic_driver_twi.h
+++ src/ic_driver_twi.h
@@ -21,28 +21,50 @@ typedef struct{
   void *nrf_twi_instance;
   ic_twi_event_cb callback;
   bool active;
+  uint8_t device_address;
   app_twi_transaction_t transaction;
   app_twi_transfer_t transfers[2];
 }ic_twi_instance_s;
 
-#define TWI_REGISTER(name)                                                        \
-  static ic_twi_instance_s name##_twi_instance;                                   \
+#define TWI_REGISTER(name, address)                                                               \
+  static ic_twi_instance_s name##_twi_instance = {.device_address};
 
 #define TWI_INIT(name)                                                            \
   ic_twi_init(&name##_twi_instance);
 
-#define TWI_SEND_DATA(name, address, in_buffer, len, callback)                    \
-  ic_twi_send(&name##_twi_instance, address, in_buffer, len, callback)
+#define TWI_UNINIT(name)                                                                          \
+  ic_twi_uninit(&name##_twi_instance);
 
-#define TWI_READ_DATA(name, address, reg_addr, in_buffer, len, callback)          \
-  ic_twi_read(&name##_twi_instance, address, reg_addr, in_buffer, len, callback)
+#define TWI_SEND_DATA(                                                                            \
+    name,                                                                                         \
+    in_buffer,                                                                                    \
+    len,                                                                                          \
+    callback)                                                                                     \
+  ic_twi_send(&name##_twi_instance, in_buffer, len, callback)
+
+#define TWI_READ_DATA(                                                                            \
+    name,                                                                                         \
+    reg_addr,                                                                                     \
+    in_buffer,                                                                                    \
+    len,                                                                                          \
+    callback)                                                                                     \
+  ic_twi_read(&name##_twi_instance, reg_addr, in_buffer, len, callback)
 
 ic_return_val_e ic_twi_init(ic_twi_instance_s * instance);
 
-ic_return_val_e ic_twi_send(ic_twi_instance_s * const instance, uint8_t address,
-    uint8_t *in_buffer, size_t len, ic_twi_event_cb callback);
+ic_return_val_e ic_twi_uninit(ic_twi_instance_s *instance);
 
-ic_return_val_e ic_twi_read(ic_twi_instance_s *const instance, uint8_t address,
-    uint8_t reg_addr, uint8_t *in_buffer, size_t len, ic_twi_event_cb callback);
+ic_return_val_e ic_twi_send(
+    ic_twi_instance_s * const instance,
+    uint8_t *in_buffer,
+    size_t len,
+    ic_twi_event_cb callback);
+
+ic_return_val_e ic_twi_read(
+    ic_twi_instance_s *const instance,
+    uint8_t reg_addr,
+    uint8_t *in_buffer,
+    size_t len,
+    ic_twi_event_cb callback);
 
 #endif /* !IC_DRIVER_TWI_H */
