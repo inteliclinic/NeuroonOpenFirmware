42584af4b8f457826c34443038cac6935af103f3
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-09-05 15:12:49 +0200

    twi macro fix

--- src/ic_driver_twi.h
+++ src/ic_driver_twi.h
@@ -53,7 +53,7 @@ typedef struct{
  *
  */
 #define TWI_REGISTER(name, address)                                                               \
-  static ic_twi_instance_s name##_twi_instance = {.device_address};
+  static ic_twi_instance_s name##_twi_instance = {.device_address = address};
 
 /**
  * @brief   Macro simplifying usage of @ref ic_twi_init.
