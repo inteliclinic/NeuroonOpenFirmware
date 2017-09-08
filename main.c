ebac95a228cf998a6e139dad36d6178dfa5cc72a
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-09-08 15:22:21 +0200

    add service to main.c file

--- main.c
+++ main.c
@@ -80,8 +80,7 @@
 #include "ic_driver_uart.h"
 #include "ic_driver_button.h"
 
-#include "ic_driver_ads_twi.h"
-#include "ic_driver_twi.h"
+#include "ic_service_ads.h"
 
 #include "ic_config.h"
 
@@ -153,12 +152,22 @@ void init_task (void *arg){
   neuroon_exti_init();
   ic_ez_ltc_module_init();
   ic_ez_ltc_glow();
+  ic_ads_service_init();
   vTaskDelete(NULL);
   taskYIELD();
 }
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, signed char *pcTaskName){
   NRF_LOG_INFO("Stack overflowed: %s\n\r", (uint32_t)pcTaskName);
+}
+
+static void  power_up_all_systems(void){
+  nrf_gpio_cfg_output(15);
+  nrf_gpio_pin_set(15);
+  nrf_gpio_cfg_output(16);
+  nrf_gpio_pin_set(16);
+  nrf_gpio_cfg_output(IC_LTC_POWER_PIN);
+  nrf_gpio_pin_set(IC_LTC_POWER_PIN);
 }
 
 /**@brief Function for application main entry.
@@ -176,8 +185,7 @@ int main(void)
       APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
     }
 
-    nrf_gpio_cfg_output(IC_LTC_POWER_PIN);
-    nrf_gpio_pin_set(IC_LTC_POWER_PIN);
+    power_up_all_systems();
 
     /*SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;*/
     NRF_LOG_INFO("starting scheduler\n");
