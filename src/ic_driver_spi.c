0e22e8d3ec6e7fe66af2124452e584f53db25fe0
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-09-13 17:25:42 +0200

    Add queue to SPI

--- src/ic_driver_spi.c
+++ src/ic_driver_spi.c
@@ -19,67 +19,150 @@
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 
+#define MAX_INSTANCES 5
+
 static struct{
   const nrf_drv_spi_t nrf_drv_instance;
   uint8_t spi_instance_cnt;
+  bool line_busy;
+  ic_spi_instance_s *instance;
   ic_spi_event_cb callback;
-}m_curren_state = {
+}m_current_state = {
                     .nrf_drv_instance = NRF_DRV_SPI_INSTANCE(IC_SPI_INSTANCE),
                     .spi_instance_cnt = 0,
+                    .line_busy        = false,
+                    .instance          = NULL,
                     .callback         = NULL
                   };
+
+static struct{
+  uint8_t tail;
+  uint8_t head;
+  ic_spi_instance_s *instance[MAX_INSTANCES];
+}m_instance_queue;
+
+static bool add_instance_to_queue(ic_spi_instance_s *instance){
+
+  if(m_instance_queue.instance[m_instance_queue.head] != NULL)
+    return false;
+
+  m_instance_queue.instance[m_instance_queue.head++] = instance;
+  if(m_instance_queue.head == MAX_INSTANCES) m_instance_queue.head = 0;
+
+  return true;
+}
+
+static ic_spi_instance_s *get_instance(void){
+
+  if(m_instance_queue.instance[m_instance_queue.tail] == NULL)
+    return NULL;
+
+  m_instance_queue.tail = m_instance_queue.tail == MAX_INSTANCES ? 0 : m_instance_queue.tail;
+  __auto_type _ret_val = m_instance_queue.instance[m_instance_queue.tail];
+  m_instance_queue.instance[m_instance_queue.tail++] = NULL;
+
+  return _ret_val;
+}
+
+static ic_spi_instance_s *show_next(){
+  return m_instance_queue.instance[m_instance_queue.tail];
+}
 
 static void spi_event_handler(nrf_drv_spi_evt_t const *p_event){
   UNUSED_VARIABLE(p_event);
 
-  if(m_curren_state.callback != (void *)0xDEADBEEF)
-    m_curren_state.callback(NULL);
+  m_current_state.line_busy = false;
 
-  m_curren_state.callback = NULL;
+  __auto_type _instance = get_instance();
+  _instance->active = false;
+
+  if (_instance->callback != NULL){
+    _instance->callback(NULL);
+  }
+
+  _instance = show_next();
+
+  if (_instance != NULL){
+    m_current_state.line_busy = true;
+    nrf_drv_spi_transfer(
+        &m_current_state.nrf_drv_instance,
+        _instance->transaction_desc.in_buffer,
+        _instance->transaction_desc.in_len,
+        _instance->transaction_desc.out_buffer,
+        _instance->transaction_desc.out_len);
+  }
 }
 
 ic_return_val_e ic_spi_init(ic_spi_instance_s *instance, uint8_t pin){
   ASSERT(instance!=NULL);
 
-  instance->nrf_spi_instance = (void *)&m_curren_state.nrf_drv_instance;
+  /*instance->nrf_spi_instance = (void *)&m_current_state.nrf_drv_instance;*/
 
   instance->pin = pin;
+  instance->active = false;
+  instance->callback = NULL;
+  memset(&instance->transaction_desc, 0, sizeof(instance->transaction_desc));
+
   nrf_gpio_cfg_output(instance->pin);
 
-  if(m_curren_state.spi_instance_cnt++ == 0){
+  if(m_current_state.spi_instance_cnt++ == 0){
     nrf_drv_spi_config_t _spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
     _spi_config.ss_pin    = NRF_DRV_SPI_PIN_NOT_USED;
     _spi_config.miso_pin  = IC_SPI_MISO_PIN;
     _spi_config.mosi_pin  = IC_SPI_MOSI_PIN;
     _spi_config.sck_pin   = IC_SPI_SCK_PIN;
-    APP_ERROR_CHECK(nrf_drv_spi_init(&m_curren_state.nrf_drv_instance, &_spi_config, spi_event_handler));
+    APP_ERROR_CHECK(nrf_drv_spi_init(&m_current_state.nrf_drv_instance, &_spi_config, spi_event_handler));
   }
 
   return IC_SUCCESS;
 }
 
-ic_return_val_e ic_spi_send(const ic_spi_instance_s *const instance, uint8_t *in_buffer,
-    size_t in_len, uint8_t *out_buffer, uint8_t out_len, ic_spi_event_cb callback){
+ic_return_val_e ic_spi_send(
+    ic_spi_instance_s *instance,
+    uint8_t *in_buffer,
+    size_t in_len,
+    uint8_t *out_buffer,
+    size_t out_len,
+    ic_spi_event_cb callback)
+{
   ASSERT(instance!=NULL);
   ASSERT(in_buffer!=NULL);
   ASSERT(out_buffer!=NULL);
 
-  if(m_curren_state.callback != NULL && m_curren_state.callback != (void *)0xDEADBEEF)
+  instance->callback = callback;
+
+  instance->transaction_desc.in_len = in_len;
+  instance->transaction_desc.in_buffer = in_buffer;
+  instance->transaction_desc.out_len = out_len;
+  instance->transaction_desc.out_buffer = out_buffer;
+
+  if(!add_instance_to_queue(instance))
     return IC_BUSY;
-
-  m_curren_state.callback = callback == NULL ? (void *)0xDEADBEEF : callback;
-
-  __auto_type _ret_val = nrf_drv_spi_transfer(instance->nrf_spi_instance, in_buffer, in_len,
-      out_buffer, out_len);
-
-  switch(_ret_val){
-    case NRF_SUCCESS:
+  else{
+    instance->active = true;
+    if(!m_current_state.line_busy){
       return IC_SUCCESS;
-    case NRF_ERROR_BUSY:
-      m_curren_state.callback = (void *)0xDEADBEEF;
-      return IC_BUSY;
-    default:
-      return IC_ERROR;
+    }
+    else{
+      m_current_state.instance = instance;
+      m_current_state.line_busy = true;
+      __auto_type _ret_val =
+        nrf_drv_spi_transfer(
+            &m_current_state.nrf_drv_instance,
+            in_buffer,
+            in_len,
+            out_buffer,
+            out_len);
+
+      switch(_ret_val){
+        case NRF_SUCCESS:
+          return IC_SUCCESS;
+        case NRF_ERROR_BUSY:
+          return IC_BUSY;
+        default:
+          return IC_ERROR;
+      }
+    }
   }
 }
 
@@ -89,4 +172,8 @@ void ic_spi_cs_high(ic_spi_instance_s *instance){
 
 void ic_spi_cs_low(ic_spi_instance_s *instance){
   nrf_gpio_pin_clear(instance->pin);
+}
+
+bool ic_spi_instance_busy(ic_spi_instance_s *instance){
+  return instance->active;
 }
--- src/ic_driver_spi.h
+++ src/ic_driver_spi.h
@@ -11,6 +11,7 @@
 #define IC_DRIVER_SPI_H
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 #include "ic_common_types.h"
 
@@ -20,16 +21,25 @@ typedef struct{
   void *nrf_spi_instance;
   ic_spi_event_cb callback;
   uint8_t pin;
+  bool active;                        /** Is line ocupated by device */
+  struct{
+    size_t in_len;
+    uint8_t *in_buffer;
+    size_t out_len;
+    uint8_t *out_buffer;
+  }transaction_desc;
 }ic_spi_instance_s;
 
 ic_return_val_e ic_spi_init(ic_spi_instance_s *instance, uint8_t pin);
 
-ic_return_val_e ic_spi_send(const ic_spi_instance_s *const instance, uint8_t *in_buffer,
-    size_t in_len, uint8_t *out_buffer, uint8_t out_len, ic_spi_event_cb callback);
+ic_return_val_e ic_spi_send(ic_spi_instance_s *instance, uint8_t *in_buffer,
+    size_t in_len, uint8_t *out_buffer, size_t out_len, ic_spi_event_cb callback);
 
 void ic_spi_cs_high(ic_spi_instance_s *instance);
 
 void ic_spi_cs_low(ic_spi_instance_s *instance);
+
+bool ic_spi_instance_busy(ic_spi_instance_s *instance);
 
 #define SPI_REGISTER(name)\
   static ic_spi_instance_s name##_spi_instance;
@@ -41,9 +51,11 @@ void ic_spi_cs_low(ic_spi_instance_s *instance);
   ic_spi_send(&name##_spi_instance, (uint8_t *)in_buffer, (uint8_t)len, (uint8_t *)out_buffer,\
       (uint8_t)len, callback)
 
-#define SPI_CS_HIGH(name) ic_spi_cs_high(&name##_spi_instance);
+#define SPI_CS_HIGH(name) ic_spi_cs_high(&name##_spi_instance)
 
-#define SPI_CS_LOW(name) ic_spi_cs_low(&name##_spi_instance);
+#define SPI_CS_LOW(name) ic_spi_cs_low(&name##_spi_instance)
+
+#define SPI_TRANSACTION_ACTIVE(name) ic_spi_instance_busy(&name##_spi_instance)
 
 #define SPI_UNINIT(name)
 
