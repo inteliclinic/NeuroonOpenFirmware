1de59a5454320022c4acdb07f723eb66051c9399
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-10-05 14:16:09 +0200

    add FreeRTOS management wrappers

--- config/ic_config.h
+++ config/ic_config.h
@@ -103,7 +103,7 @@
 
 #define IC_TWI_SDA_PIN          28
 #define IC_TWI_SCL_PIN          29
-#define IC_TWI_IRQ_PRIORITY     IC_IRQ_PRIORITY_MID
+#define IC_TWI_IRQ_PRIORITY     IC_IRQ_PRIORITY_HIGH
 #define IC_TWI_FREQUENCY        IC_TWI_400KHZ_FREQUENCY
 
 #define IC_TWI_PENDIG_TRANSACTIONS  5
@@ -138,38 +138,79 @@ static inline int isr_context(){
 
 #ifdef SEMAPHORE_H
 
-#define ALLOCK_SEMAPHORE(name) static SemaphoreHandle_t name = NULL
+#define ALLOCK_SEMAPHORE(name)                                              \
+  static SemaphoreHandle_t name##_semaphore = NULL
 
-#define INIT_SEMAPHORE_BINARY(name)\
-  name = xSemaphoreCreateBinary()
+#define INIT_SEMAPHORE_BINARY(name)                                         \
+  name##_semaphore = xSemaphoreCreateBinary()
 
-#define TAKE_SEMAPHORE(name, time_ms)                             \
-  do{                                                             \
-    if(isr_context()){                                            \
-      __auto_type higher_priority_task_woken = pdFALSE;           \
-      xSemaphoreTakeFromISR(name, &higher_priority_task_woken);   \
-      portYIELD_FROM_ISR(higher_priority_task_woken);             \
-    }                                                             \
-    else{                                                         \
-      xSemaphoreTake(name, time_ms);                              \
-    }                                                             \
+#define TAKE_SEMAPHORE(name, time_ms, ret_val)                              \
+  do{                                                                       \
+    if(isr_context()){                                                      \
+      __auto_type higher_priority_task_woken = pdFALSE;                     \
+      ret_val = xSemaphoreTakeFromISR(name##_semaphore,                     \
+          &higher_priority_task_woken);                                     \
+      portYIELD_FROM_ISR(higher_priority_task_woken);                       \
+    }                                                                       \
+    else{                                                                   \
+      ret_val = xSemaphoreTake(name##_semaphore, time_ms);                  \
+    }                                                                       \
   }while(0)
 
-#define GIVE_SEMAPHORE(name)                                      \
-  do{                                                             \
-    if(isr_context()){                                            \
-      __auto_type higher_priority_task_woken = pdFALSE;           \
-      xSemaphoreGiveFromISR(name, &higher_priority_task_woken);   \
-      portYIELD_FROM_ISR(higher_priority_task_woken);             \
-    }                                                             \
-    else{                                                         \
-      xSemaphoreGive(name);                                       \
-    }                                                             \
+#define GIVE_SEMAPHORE(name)                                                \
+  do{                                                                       \
+    if(isr_context()){                                                      \
+      __auto_type higher_priority_task_woken = pdFALSE;                     \
+      xSemaphoreGiveFromISR(name##_semaphore, &higher_priority_task_woken); \
+      portYIELD_FROM_ISR(higher_priority_task_woken);                       \
+    }                                                                       \
+    else{                                                                   \
+      xSemaphoreGive(name##_semaphore);                                     \
+    }                                                                       \
   }while(0)
-
-
 
 #endif /* SEMAPHORE_H */
 
+#ifdef INC_TASK_H
+#define RESUME_TASK(task)                                                   \
+  do{                                                                       \
+    if(isr_context()){                                                      \
+      __auto_type task##_yield_required = xTaskResumeFromISR(task);         \
+      portYIELD_FROM_ISR(task##_yield_required);                            \
+    }                                                                       \
+    else{                                                                   \
+      vTaskResume(task);                                                    \
+    }                                                                       \
+  }while(0)
+
+#endif /* INC_TASK_H */
+
+#ifdef TIMERS_H
+
+#define START_TIMER(timer, block_time, ret_val)                             \
+  do{                                                                       \
+    if(isr_context()){                                                      \
+      __auto_type timer##_yield_required = pdFALSE;                         \
+      ret_val = xTimerStartFromISR(timer, &timer##_yield_required);         \
+      portYIELD_FROM_ISR(timer##_yield_required);                           \
+    }                                                                       \
+    else{                                                                   \
+      ret_val = xTimerStart(timer, block_time);                             \
+    }                                                                       \
+  }while(0)
+
+#define STOP_TIMER(timer, block_time, ret_val)                              \
+  do{                                                                       \
+    if(isr_context()){                                                      \
+      __auto_type timer##_yield_required = pdFALSE;                         \
+      ret_val = xTimerStopFromISR(timer, &timer##_yield_required);          \
+      portYIELD_FROM_ISR(timer##_yield_required);                           \
+    }                                                                       \
+    else{                                                                   \
+      ret_val = xTimerStop(timer, block_time);                              \
+    }                                                                       \
+  }while(0)
+
+#endif /* TIMERS_H */
 
 #endif /* !IC_CONFIG_H */
