c15e0d667e8e35b99da3e07dfcd2d7e6f0c16d7a
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2017-12-11 12:11:52 +0100

    add ads refresh TWI

--- src/ic_driver_ads.c
+++ src/ic_driver_ads.c
@@ -143,8 +143,10 @@ ic_return_val_e ads_get_value(void (*p_read_callback)(int16_t), bool force){
   else
     return IC_BUSY;
 
+  __auto_type _ret_val = IC_ERROR;
+
   if(force){
-    __auto_type _ret_val = TWI_READ_DATA(
+    _ret_val = TWI_READ_DATA(
         ADS,
         ADS_ADDR_CONV_REG,
         (uint8_t *)&m_conversion_read_frame,
@@ -157,10 +159,8 @@ ic_return_val_e ads_get_value(void (*p_read_callback)(int16_t), bool force){
       m_user_read_callback(SWAP_2_BYTES(m_conversion_read_frame));
       m_user_read_callback = NULL;
     }
-
-    return _ret_val;
   }else{
-    return TWI_READ_DATA(
+    _ret_val = TWI_READ_DATA(
         ADS,
         ADS_ADDR_CONV_REG,
         (uint8_t *)&m_conversion_read_frame,
@@ -168,8 +168,14 @@ ic_return_val_e ads_get_value(void (*p_read_callback)(int16_t), bool force){
         m_read_value_cb,
         NULL
         );
-}
   }
+
+  if(_ret_val != IC_SUCCESS){
+    ic_twi_refresh_bus();
+  }
+
+  return _ret_val;
+}
 
 /**@@fn ads_change_gain ()
  * @brief 			Amplifier will be set with provided gain
