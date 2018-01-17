22c9234ee1cda29d4239377d437029fba2c1b394
Wojtek <w.weclewski@inteliclinic.com>
2018-01-17 15:31:35 +0100

    Replace afe and acc service with stream1 service

---
+++ src/ic_service_stream1.h
@@ -0,0 +1,18 @@
+/**
+ * @file    ic_service_stream1.h
+ * @author  Wojtek Węclewski <w.weclewski@inteliclinic.com>
+ * @date    January, 2018
+ * @brief   Brief description
+ *
+ * Description
+ */
+
+#ifndef IC_SERVICE_STREAM1_H
+#define IC_SERVICE_STREAM1_H
+
+#include "ic_config.h"
+
+ic_return_val_e ic_service_stream1_init(void);
+ic_return_val_e ic_service_stream1_deinit(void);
+
+#endif /* !IC_SERVICE_STREAM1_H */
