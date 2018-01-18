155116e4690df7caf7ed91e13ae5fb93aee2983a
Pawel Kazmierzewski <p.kazmierzewski@inteliclinic.com>
2018-01-18 14:37:17 +0100

    ADS, remove potential deadlock

--- src/ic_service_ads.c
+++ src/ic_service_ads.c
@@ -31,15 +31,30 @@ static TimerHandle_t m_ads_service_timer_handle = NULL;
 static bool m_module_initialized = false;
 
 static volatile uint8_t m_measurement_cnt = 0;
+u_eegDataFrameContainter m_eeg_packet;
 
-static int16_t volatile m_eeg_measurement;
 static TaskHandle_t send_data_task_handle = NULL;
 
 ALLOCK_SEMAPHORE(m_twi_ready);
 
+static inline bool add_eeg(int16_t eeg){
+  if(m_measurement_cnt == 0)
+    m_eeg_packet.frame.time_stamp = GET_TICK_COUNT();
+
+  m_eeg_packet.frame.eeg_data[m_measurement_cnt++] = eeg;
+  if(m_measurement_cnt ==
+      sizeof(m_eeg_packet.frame.eeg_data)/sizeof(m_eeg_packet.frame.eeg_data[0]))
+  {
+    m_measurement_cnt = 0;
+    return true;
+  }
+  return false;
+}
+
 void read_callback(int16_t eeg){
-  m_eeg_measurement = eeg;
-  RESUME_TASK(send_data_task_handle);
+  GIVE_SEMAPHORE(m_twi_ready);
+  if(add_eeg(eeg))
+    RESUME_TASK(send_data_task_handle);
 }
 
 static void on_stream_state_change(bool active){
@@ -57,7 +72,7 @@ static void ads_timer_callback(TimerHandle_t xTimer){
   UNUSED_PARAMETER(xTimer);
 
   __auto_type _semphr_successfull = pdTRUE;
-  TAKE_SEMAPHORE(m_twi_ready, IC_ADS_TICK_PERIOD, _semphr_successfull);
+  TAKE_SEMAPHORE(m_twi_ready, 0, _semphr_successfull);
   if(_semphr_successfull == pdFALSE){
     NRF_LOG_INFO("Could not take TWI transaction semaphore\n");
   }
@@ -83,47 +98,31 @@ static void ads_timer_callback(TimerHandle_t xTimer){
 }
 
 static void send_data_task(void *arg){
-  u_eegDataFrameContainter eeg_packet;
-  bool send_via_ble = false;
-
   uint32_t _nrf_error;
-
   for(;;){
-    if(send_via_ble){
-      __auto_type _err = ble_iccs_send_to_stream0(
-          eeg_packet.raw_data,
-          sizeof(u_eegDataFrameContainter),
-          &_nrf_error);
-
-      switch(_err){
-        case IC_SUCCESS:
-          send_via_ble = false;
-          break;
-        case IC_BLE_NOT_CONNECTED:
-          send_via_ble = false;
-          break;
-        case IC_BUSY:
-          NRF_LOG_INFO("Ależ dupa!\n");
-          continue; // TODO: Fix it. Can kill CPU.
-        default:
-          /*NRF_LOG_INFO("err: %s\n", (uint32_t)ic_get_nrferr2str(_nrf_error));*/
-          break;
-      }
+    __auto_type _err = ble_iccs_send_to_stream0(
+        m_eeg_packet.raw_data,
+        sizeof(u_eegDataFrameContainter),
+        &_nrf_error);
+    switch(_err){
+      case IC_SUCCESS:
+        NRF_LOG_INFO("%d:%X:%X:%X:%X\n",
+            m_eeg_packet.frame.time_stamp,
+            *(uint32_t *)&m_eeg_packet.frame.eeg_data[0],
+            *(uint32_t *)&m_eeg_packet.frame.eeg_data[2],
+            *(uint32_t *)&m_eeg_packet.frame.eeg_data[4],
+            *(uint32_t *)&m_eeg_packet.frame.eeg_data[6]
+            );
+        break;
+      case IC_BLE_NOT_CONNECTED:
+        break;
+      case IC_BUSY:
+        NRF_LOG_INFO("Ależ dupa!\n");
+        continue; // TODO: Fix it. Can kill CPU.
+      default:
+        /*NRF_LOG_INFO("err: %s\n", (uint32_t)ic_get_nrferr2str(_nrf_error));*/
+        break;
     }
-    else{
-      if (m_measurement_cnt == 0)
-        eeg_packet.frame.time_stamp = GET_TICK_COUNT();
-
-      eeg_packet.frame.eeg_data[m_measurement_cnt++] = m_eeg_measurement;
-      GIVE_SEMAPHORE(m_twi_ready);
-
-      if(m_measurement_cnt >= sizeof(eeg_packet.frame.eeg_data)/sizeof(eeg_packet.frame.eeg_data[0])){
-        send_via_ble = true;
-        m_measurement_cnt = 0;
-        continue;
-      }
-    }
-
     vTaskSuspend(NULL);
     taskYIELD();
   }
